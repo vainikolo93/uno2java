88756ac1f22af8034ac3df26e125d67749079044
mutantveggie <mutantveggie@3873034e-f493-11dd-8c4e-4bbb75a408b7>
2009-02-13 04:14:09 +0000

    didn't commit properly, here are the java files for real =)

---
+++ trunk/unoToJava/Main.java
@@ -0,0 +1,33 @@
+import javax.swing.JFrame;
+
+
+public class Main 
+{
+	//@SuppressWarnings("null")
+	@SuppressWarnings("null")
+	public static void main(String[] args)
+	{
+
+		JFrame jf = new JFrame("title");
+		jf.setSize(400, 200);
+		//NeatWindow n = new NeatWindow();
+		//jf.getContentPane().add(n);
+		// need to be very specific about key listening...
+		//jf.addKeyListener(n);
+		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jf.setVisible(true);
+		
+		
+		System.out.println("working?");
+		
+		//Player p1 = null;
+		//p1.setName();
+		//p1.printName();
+		
+		//String filename = "unoDeck.txt";
+		//Deck drawPile;// = null;
+		//drawPile.load(filename);
+		//drawPile.debug();
+		
+	}
+}
