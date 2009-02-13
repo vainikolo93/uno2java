56c55516b4e6207ed6024c09523d36896b787de7
mutantveggie <mutantveggie@3873034e-f493-11dd-8c4e-4bbb75a408b7>
2009-02-13 04:14:09 +0000

    didn't commit properly, here are the java files for real =)

---
+++ unoToJava/Game.java
@@ -0,0 +1,15 @@
+
+
+//handles the data for the cursor
+class cursor
+{
+	public int m_x, m_y;
+	public int m_minX, m_maxX, m_minY, m_maxY;
+	char m_icon;
+	int m_lookAt;
+}
+
+public class Game 
+{
+
+}
